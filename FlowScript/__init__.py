548124469bf18cee5fc9842ec5f992152e704020
QDYG <thanglinh868686@gmail.com>
2026-02-26 06:43:56 +0700

    Refactor FlowScript architecture from tree-walk to bytecode

--- FlowScript/__init__.py
+++
@@ -1,8 +0,0 @@
-from .environment import Environment
-from .executor import Executor
-from .lexer import Lexer
-from .main import main
-from .parser import Parser
-from .transformer_ast import Transformer
-
-__all__ = ['main', 'Lexer', 'Parser', 'Transformer', 'Executor', 'Environment']
